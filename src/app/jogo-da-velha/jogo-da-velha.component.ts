b3b86b156fea32f2538bd8a082485618c11c4097
Andre Viana <code.andre3@gmail.com>
2021-07-08 22:13:28 -0300

    edit html to service/components

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { JogoDaVelhaService } from './shared/jogo-da-velha.service';
+
 @Component({
   selector: 'app-jogo-da-velha',
   templateUrl: './jogo-da-velha.component.html',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoDaVelhaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { //ao iniciar vai sempre executar a função inicializar
+    this.jogoDaVelhaService.inicializar();
+  }
+
+  get mostrarInicio(): boolean { 
+    return this.jogoDaVelhaService.mostrarInicio;
+  }
+
+  get mostrarTabuleiro(): boolean {
+    return this.jogoDaVelhaService.mostrarTabuleiro;
+  }
+
+  get mostrarFim(): boolean {
+    return this.jogoDaVelhaService.mostrarFim;
+  }
+
+  iniciarJogo(): void {
+    this.jogoDaVelhaService.iniciarJogo(); //ligado no service
+  }
+
+  jogar(posX: number, posY: number): void {
+    this.jogoDaVelhaService.jogar(posX, posY);
+  }
+
+  exibirX(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirX(posX, posY);
+  }
+
+  exibirO(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirO(posX, posY);
+  }
+
+  exibirVitoria(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirVitoria(posX, posY);
   }
 
 }
